e9a13c384ce951e621bdbe2acde6dca3693c0e81
arthurgauffre <arth.gauffre@gmail.com>
2024-11-23 14:02:03 +0100

    [ADD] enhance Go backend with Gin framework and add unit tests using Testify

--- poc/backend/go/project/main.go
+++ poc/backend/go/project/main.go
@@ -2,20 +2,26 @@ package main
 
 import "github.com/gin-gonic/gin"
 
-func main() {
-	server := gin.Default()
+func setupRouter() *gin.Engine {
+	router := gin.Default()
 
-	server.GET("/ping", func(c *gin.Context) {
+	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
 
-	server.GET(":word", func(c *gin.Context) {
+	router.GET("/:word", func(c *gin.Context) {
 		word := c.Param("word")
 		c.JSON(200, gin.H{
 			"message": word,
 		})
 	})
-	server.Run(":8080")
+
+	return router
+}
+
+func main() {
+	router := setupRouter()
+	router.Run(":8080")
 }
