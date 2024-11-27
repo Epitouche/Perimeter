62f069fb58eda5acb4c38ead15e6a8ceca29b95b
hope <anthony.ardhuin@epitech.eu>
2024-11-27 13:03:41 +0000

    [ADD] - Routing
    Adding all routes, basic navbar for routes that need it, and error pages

---
+++ frontend/error.vue
@@ -0,0 +1,34 @@
+<template>
+  <NuxtLayout>
+    <div class="relative block w-full border-0 rounded-m text-gray-900 text-center py-2">
+        <template v-if="error.statusCode === 404">
+          <h1 style="font-size: 6rem; font-weight: bold;">404!</h1>
+          <p>Sorry, that page doesn't exist.</p>
+        </template>
+            <template v-else>
+          <h1 style="font-size: 6rem; font-weight: bold;">Dang!</h1>
+                <p>
+                  <strong>{{ error.message }}</strong>
+                </p>
+          <p>It looks like something broke.</p>
+          <p>Sorry about that.</p>
+      </template>
+      <p>
+            Go back to your 
+            <a @click="handleError" class="font-bold underline">
+              areas.
+            </a>
+          </p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+const error = useError();
+
+const handleError = () => {
+  clearError({
+    redirect: '/myareas',
+  });
+};
+</script>
---
+++ frontend/pages/myareas.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavigationBar />
+    My Areas
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
