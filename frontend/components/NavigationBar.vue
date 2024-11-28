46d52aa9e255feabedb3f1c349160523d48586e7
hope <anthony.ardhuin@epitech.eu>
2024-11-28 15:31:47 +0000

    [ADD] - Style + NuxtLink
    Adding style to the navbar, and changing aref to NuxtLink for routes

--- frontend/components/NavigationBar.vue
+++ frontend/components/NavigationBar.vue
@@ -1,24 +1,83 @@
 <template>
-  <UHorizontalNavigation :links="links" class="border-b border-gray-900 dark:border-gray-900" />
-  <div>
+  <div class="navbar">
+    <div class="logo">
+      <img src="../public/PerimeterIcon.png" alt="Logo" class="logo-image" />
+      <span class="title">Perimeter</span>
+    </div>
+
+    <div class="nav-links">
+      <nav>
+        <ul class="nav-list">
+          <li>
+            <NuxtLink to="/workflow" class="nav-link">Workflow</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/myareas" class="nav-link">My Areas</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/services" class="nav-link">My Services</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+    <div class="profile">
+      <div class="profile-icon"></div>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup>
-  const links = [{
-    label: 'Workflow',
-    icon: 'i-heroicons-home',
-    to: '/workflow'
-  }, {
-    label: 'My Area',
-    icon: 'i-heroicons-home',
-    to: '/myareas'
-  }, {
-    label: 'My Services',
-    icon: 'i-heroicons-chart-bar',
-    to: '/services'
-  }]
-</script>
-
 <style>
+.navbar {
+  display: flex;
+  align-items: center;
+  padding: 1.5rem;
+  background-color: white;
+}
+
+.logo {
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  flex-grow: 0.95;
+}
+
+.logo-image {
+  height: 4rem;
+  width: 4rem;
+}
+
+.title {
+  font-size: 2rem;
+  font-weight: 900;
+}
+
+.nav-list {
+  display: flex;
+  gap: 2.5rem;
+}
+
+.nav-link {
+  color: black;
+  text-decoration: none;
+  font-size: 1.25rem;
+  font-weight: 700;
+}
+
+.nav-link:hover {
+  text-decoration: underline;
+}
+
+.profile {
+  margin-left: auto;
+}
+
+.profile-icon {
+  height: 4rem;
+  width: 4rem;
+  background-color: #d1d5db;
+  border-radius: 50%;
+  border: 2px solid black;
+}
+
 </style>
