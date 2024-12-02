494da8490671c115ce15a4e9b69c70d88d56a19a
hope <anthony.ardhuin@epitech.eu>
2024-12-02 10:05:57 +0000

    [UPD] Profile Button, style
    Adding the profil button with a link to setting or 'temp logout' going to /login

--- frontend/components/NavigationBar.vue
+++ frontend/components/NavigationBar.vue
@@ -1,13 +1,23 @@
+<script lang="ts" setup>
+  import { ref } from "vue";
+
+  const menuOpen = ref(false);
+
+  function toggleMenu() {
+    menuOpen.value = !menuOpen.value;
+  }
+</script>
+
 <template>
-  <div class="navbar">
-    <div class="logo">
-      <img src="../public/PerimeterIcon.png" alt="Logo" class="logo-image" />
-      <span class="title">Perimeter</span>
+  <div class="flex items-center p-[1.5em] bg-custom_color-bg_section">
+    <div class="flex items-center gap-[1em] grow-[0.95]">
+      <img src="../public/PerimeterIcon.png" alt="perimeter-icon" class="h-[4em] w-[4em]" />
+      <span class="font-black text-[2em]">Perimeter</span>
     </div>
 
     <div class="nav-links">
       <nav>
-        <ul class="nav-list">
+        <ul class="flex gap-[2.5em]">
           <li>
             <NuxtLink to="/workflow" class="nav-link">Workflow</NuxtLink>
           </li>
@@ -21,41 +31,28 @@
       </nav>
     </div>
 
-    <div class="profile">
-      <div class="profile-icon"></div>
+    <div class="ml-auto relative">
+      <button class="h-[4em] w-[4em] bg-gray-300 rounded-full border-custom_border_width border-black cursor-pointer" @click="toggleMenu"></button>
+      <div v-if="menuOpen" class="absolute top-full mt-4 right-0 p-4 rounded shadow-md flex flex-col gap-4 min-w-[200px] z-[1000] bg-custom_color-bg_section">
+        <div class="menu-header flex items-center justify-between gap-[1em]">
+          <span class="font-[400] text-[1em]">Username</span>
+          <div class="h-[3em] w-[3em] bg-gray-300 rounded-full border-custom_border_width border-black "></div>
+        </div>
+
+        <NuxtLink to="/settings" class="nav-link">Settings</NuxtLink>
+
+        <UButton class="flex items-center gap-2 py-2 px-4 text-base font-bold rounded-custom_border_radius cursor-pointer bg-custom_color-bg_section logout-button">
+          <svg class="w-[1em] h-[1em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M28 16H8m12-8l8 8l-8 8m-9 4H3V4h8"/>
+          </svg>
+          <NuxtLink to="/login">Logout</NuxtLink>
+        </UButton>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
-.navbar {
-  display: flex;
-  align-items: center;
-  padding: 1.5rem;
-  background-color: white;
-}
-
-.logo {
-  display: flex;
-  align-items: center;
-  gap: 1rem;
-  flex-grow: 0.95;
-}
-
-.logo-image {
-  height: 4rem;
-  width: 4rem;
-}
-
-.title {
-  font-size: 2rem;
-  font-weight: 900;
-}
-
-.nav-list {
-  display: flex;
-  gap: 2.5rem;
-}
 
 .nav-link {
   color: black;
@@ -68,16 +65,12 @@
   text-decoration: underline;
 }
 
-.profile {
-  margin-left: auto;
+.logout-button {
+  background-color: #ef4444;
 }
 
-.profile-icon {
-  height: 4rem;
-  width: 4rem;
-  background-color: #d1d5db;
-  border-radius: 50%;
-  border: 2px solid black;
+.logout-button:hover {
+  background-color: #dc2626;
 }
 
 </style>
