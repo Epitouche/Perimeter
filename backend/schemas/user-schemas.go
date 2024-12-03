7242266609b52a4be707964891c33f83092e5d74
Tom Mendy <tom.mendy@epitech.eu>
2024-12-03 14:20:16 +0100

    [UPD] refactor user ID handling in controllers and services

--- backend/schemas/user-schemas.go
+++ backend/schemas/user-schemas.go
@@ -5,11 +5,12 @@ import (
 )
 
 type User struct {
-	Id        uint64    `gorm:"primaryKey;autoIncrement"  json:"id,omitempty"`
-	Username  string    `gorm:"type:varchar(100);unique"  json:"username"     binding:"required"`
-	Email     string    `gorm:"type:varchar(100);unique"  json:"email"        binding:"requiredcredentials"`
-	Password  string    `gorm:"type:varchar(100)"         json:"password"`  // can be null for Oauth2.0 users
-	GithubId  uint64    `gorm:"default:null"              json:"github_id"` // Foreign key for Github
-	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"createdAt"`
-	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updated_at"`
+	Id        uint64    `gorm:"primaryKey;autoIncrement"         json:"id,omitempty"`
+	Username  string    `gorm:"type:varchar(100);unique"         json:"username"     binding:"required"`
+	Email     string    `gorm:"type:varchar(100);unique"         json:"email"        binding:"requiredcredentials"`
+	Password  string    `gorm:"type:varchar(100)"                json:"password"` // can be null for Oauth2.0 users
+	TokenId   uint64    `                                        json:"-"`        // Foreign key for LinkUrl
+	Token     Token     `gorm:"foreignKey:TokenId;references:Id" json:"token_id"`
+	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"        json:"createdAt"`
+	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"        json:"updated_at"`
 }
