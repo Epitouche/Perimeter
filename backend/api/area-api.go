6cfe5d61d63ddd5605982a9a18b932602b7cb4a8
Tom Mendy <tom.mendy@epitech.eu>
2024-12-06 11:24:00 +0100

    [UPD] Enhance Area API Add JWT authorization middleware and improve error response format

--- backend/api/area-api.go
+++ backend/api/area-api.go
@@ -1,9 +1,13 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 
 	"area/controller"
+	"area/middlewares"
+	"area/schemas"
 )
 
 type AreaApi struct {
@@ -11,7 +15,7 @@ type AreaApi struct {
 }
 
 func NewAreAPI(controller controller.AreaController, apiRoutes *gin.RouterGroup) *AreaApi {
-	apiRoutes = apiRoutes.Group("/area")
+	apiRoutes = apiRoutes.Group("/area", middlewares.AuthorizeJWT())
 	api := AreaApi{
 		controller: controller,
 	}
@@ -23,9 +27,11 @@ func (api *AreaApi) CreateArea(apiRoutes *gin.RouterGroup) {
 	apiRoutes.POST("/", func(ctx *gin.Context) {
 		response, err := api.controller.CreateArea(ctx)
 		if err != nil {
-			ctx.JSON(500, gin.H{"error": err.Error()})
+			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorRespose{
+				Error: err.Error(),
+			})
 			return
 		}
-		ctx.JSON(200, response)
+		ctx.JSON(http.StatusOK, response)
 	})
 }
