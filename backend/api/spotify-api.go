b4ce7035ff0e9f6557589459f1c1b6d8e0f170f2
Tom Mendy <tom.mendy@epitech.eu>
2024-12-06 11:39:14 +0100

    [FIX] Correct typo in ErrorResponse struct and update references across the codebase

--- backend/api/spotify-api.go
+++ backend/api/spotify-api.go
@@ -37,13 +37,13 @@ func NewSpotifyAPI(
 //	@Accept			json
 //	@Produce		json
 //	@Success		200	{object}	schemas.AuthenticationUrl
-//	@Failure		500	{object}	schemas.ErrorRespose
+//	@Failure		500	{object}	schemas.ErrorResponse
 //	@Router			/spotify/auth [get]
 func (api *SpotifyAPI) RedirectToService(apiRoutes *gin.RouterGroup) {
 	apiRoutes.GET("/auth", func(ctx *gin.Context) {
 		authURL, err := api.controller.RedirectToService(ctx, apiRoutes.BasePath()+"/auth/callback")
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorRespose{
+			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorResponse{
 				Error: err.Error(),
 			})
 		} else {
@@ -61,7 +61,7 @@ func (api *SpotifyAPI) RedirectToService(apiRoutes *gin.RouterGroup) {
 //	@Produce		json
 //	@Param			payload	body		schemas.CodeCredentials	true	"Callback Payload"
 //	@Success		200		{object}	schemas.JWT
-//	@Failure		500		{object}	schemas.ErrorRespose
+//	@Failure		500		{object}	schemas.ErrorResponse
 //	@Router			/spotify/auth/callback [post]
 func (api *SpotifyAPI) HandleServiceCallback(apiRoutes *gin.RouterGroup) {
 	apiRoutes.POST("/auth/callback", func(ctx *gin.Context) {
@@ -70,7 +70,7 @@ func (api *SpotifyAPI) HandleServiceCallback(apiRoutes *gin.RouterGroup) {
 			apiRoutes.BasePath()+"/auth/callback",
 		)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorRespose{
+			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorResponse{
 				Error: err.Error(),
 			})
 		} else {
@@ -89,14 +89,14 @@ func (api *SpotifyAPI) HandleServiceCallback(apiRoutes *gin.RouterGroup) {
 //	@Security		Bearer
 //	@Param			Authorization	header		string	true	"Bearer token"
 //	@Success		200				{object}	schemas.UserCredentials
-//	@Failure		401				{object}	schemas.ErrorRespose
-//	@Failure		500				{object}	schemas.ErrorRespose
+//	@Failure		401				{object}	schemas.ErrorResponse
+//	@Failure		500				{object}	schemas.ErrorResponse
 //	@Router			/spotify/info/user [get]
 func (api *SpotifyAPI) GetUserInfo(apiRoutes *gin.RouterGroup) {
 	apiRoutes.GET("/user", func(ctx *gin.Context) {
 		usetInfo, err := api.controller.GetUserInfo(ctx)
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorRespose{
+			ctx.JSON(http.StatusInternalServerError, &schemas.ErrorResponse{
 				Error: err.Error(),
 			})
 		} else {
