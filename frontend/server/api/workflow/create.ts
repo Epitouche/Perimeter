8392782f24b2e8065534ef87b89f979800ad4fd0
Alea Chlodnik <achlodnik@gmail.com>
2024-12-08 14:55:46 +0000

    [UPD] api/workflow/create.ts
    send bearer token

--- frontend/server/api/workflow/create.ts
+++ frontend/server/api/workflow/create.ts
@@ -2,6 +2,7 @@ export default defineEventHandler(async (event) => {
   try {
     const params = await readBody(event);
     if (
+      !params.token ||
       !params.actionOptions ||
       !params.actionId ||
       !params.reactionOptions ||
@@ -14,6 +15,9 @@ export default defineEventHandler(async (event) => {
     }
     const response = await $fetch(`http://server:8080/api/v1/area`, {
       method: "POST",
+      headers: {
+        Authorization: "Bearer " + params.token,
+      },
       body: {
         actionOptions: params.actionOptions,
         actionId: params.actionId,
