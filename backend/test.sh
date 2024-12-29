703fde8b2571c63fc9a297abfe5076c7472774e8
Tom Mendy <tom.mendy@epitech.eu>
2024-12-29 17:52:54 +0100

    [UPD] update .gitignore and enhance test coverage reporting

--- backend/test.sh
+++ backend/test.sh
@@ -3,5 +3,8 @@
 set -e
 set -a
 source ../.env
-go test -cover -coverprofile=coverage.out -race ./...
+go test -cover -race -parallel 10 -coverprofile=cover.out ./...
+go tool cover -html cover.out -o cover.html
+go install github.com/nikolaydubina/go-cover-treemap@latest
+go-cover-treemap -coverprofile cover.out >cover.svg
 set +a
