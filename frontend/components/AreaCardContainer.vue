39ee176438aac4d09e46ac1a79e0c399bbaeac66
Alea Chlodnik <achlodnik@gmail.com>
2025-01-08 17:06:01 +0100

    [UPD] AreaCardContainer.vue
    delete button is now functional

--- frontend/components/AreaCardContainer.vue
+++ frontend/components/AreaCardContainer.vue
@@ -34,7 +34,7 @@ const onDelete = async (areaId: number) => {
         method: "POST",
         body: {
           token: token.value,
-          id: areaId,
+          areaId: areaId,
         },
       });
       console.log("response:", response);
@@ -45,12 +45,13 @@ const onDelete = async (areaId: number) => {
         console.error("An unknown error occurred", error);
       }
     }
+    toggleConfirmDeletionModal(areaId);
     componentKey.value += 1;
-    // toggleConfirmDeletionModal(areaId);
     return;
   }
   toggleConfirmDeletionModal(areaId);
   toggleAreaModal(areaId);
+  return;
 };
 
 const cancelDeletion = (areaId: number) => {
@@ -69,81 +70,43 @@ onMounted(() => {
 
 <template>
   <UContainer
-    :key="componentKey"
-    :ui="{ padding: '!px-0', constrained: 'max-w-full max-h-full' }"
-    class="flex flex-row justify-center items-center gap-10 flex-wrap py-5 w-full h-full"
-  >
+  :key="componentKey" :ui="{ padding: '!px-0', constrained: 'max-w-full max-h-full' }"
+    class="flex flex-row justify-center items-center gap-10 flex-wrap py-5 w-full h-full">
     <div v-for="area in areas" :key="area.id">
-      <UContainer
-        :ui="{ padding: 'px-0', constrained: 'max-w-none' }"
+      <UContainer :ui="{ padding: 'px-0', constrained: 'max-w-none' }"
         class="flex flex-col justify-center items-center text-white font-extrabold text-6xl rounded-custom_border_radius w-[5em] h-[4.5em]"
-        :style="{ backgroundColor: area.action.service.color }"
-        @click="toggleAreaModal(area.id)"
-      >
-        <h2
-          class="clamp-2-lines capitalize text-4xl text-center break-words pb-2 w-full"
-        >
+        :style="{ backgroundColor: area.action.service.color }" @click="toggleAreaModal(area.id)">
+        <h2 class="clamp-2-lines capitalize text-4xl text-center break-words pb-2 w-full">
           {{ formatName(area.action.name) }}
         </h2>
         <div class="grid place-items-center h-36 relative w-full">
-          <img
-            :src="area.action.service.icon"
-            :alt="area.action.service.name"
-            class="w-24 h-24 p-0 absolute top-1 left-12"
-          />
-          <img
-            :src="area.reaction.service.icon"
-            :alt="area.reaction.service.name"
-            class="w-24 h-24 p-0 absolute bottom-0 right-12"
-          />
+          <img :src="area.action.service.icon" :alt="area.action.service.name"
+            class="w-24 h-24 p-0 absolute top-1 left-12">
+          <img :src="area.reaction.service.icon" :alt="area.reaction.service.name"
+            class="w-24 h-24 p-0 absolute bottom-0 right-12">
         </div>
       </UContainer>
-      <UModal
-        v-model="areaIsOpen[area.id]"
-        :ui="{
-          width: 'w-2/5',
-        }"
-      >
-        <div
-          class="flex flex-col gap-14 font-semibold text-white rounded-custom_border_radius pl-20 pr-12 py-10 w-full"
-          :style="{ backgroundColor: area.action.service.color }"
-        >
+      <UModal v-model="areaIsOpen[area.id]" :ui="{
+        width: 'w-2/5',
+      }">
+        <div class="flex flex-col gap-14 font-semibold text-white rounded-custom_border_radius pl-20 pr-12 py-10 w-full"
+          :style="{ backgroundColor: area.action.service.color }">
           <div class="flex flex-row justify-between pb-2 w-full">
             <h2 class="text-6xl text-center w-full"><b>Temp title</b></h2>
-            <UButton
-              variant="ghost"
-              class="self-end w-fit"
-              @click="toggleAreaModal(area.id)"
-            >
+            <UButton variant="ghost" class="self-end w-fit" @click="toggleAreaModal(area.id)">
               <UIcon name="i-bytesize-close" class="w-12 h-12 text-white" />
             </UButton>
           </div>
-          <div
-            class="capitalize self-start flex flex-row items-center text-5xl gap-5"
-          >
-            <img
-              :src="area.action.service.icon"
-              :alt="area.action.service.name"
-              class="w-16 h-16 p-0"
-            />
-            />
+          <div class="capitalize self-start flex flex-row items-center text-5xl gap-5">
+            <img :src="area.action.service.icon" :alt="area.action.service.name" class="w-16 h-16 p-0">
             <p>
-              <b>{{ area.action.service.name }}</b
-              >: {{ formatName(area.action.name) }}
+              <b>{{ area.action.service.name }}</b>: {{ formatName(area.action.name) }}
             </p>
           </div>
-          <div
-            class="capitalize self-start flex flex-row items-center text-5xl gap-5"
-          >
-            <img
-              :src="area.reaction.service.icon"
-              :alt="area.reaction.service.name"
-              class="w-16 h-16 p-0"
-            />
-            />
+          <div class="capitalize self-start flex flex-row items-center text-5xl gap-5">
+            <img :src="area.reaction.service.icon" :alt="area.reaction.service.name" class="w-16 h-16 p-0">
             <p>
-              <b>{{ area.reaction.service.name }}</b
-              >:
+              <b>{{ area.reaction.service.name }}</b>:
               {{ formatName(area.reaction.name) }}
             </p>
           </div>
@@ -151,43 +114,27 @@ onMounted(() => {
             <b>Description</b>: Desc will go here
           </p>
           <UTooltip text="Delete" class="self-end w-fit">
-            <UButton
-              variant="ghost"
-              class="hover_underline_animation items-end w-fit p-0 pb-1"
-              @click="onDelete(areaId)"
-            >
+            <UButton variant="ghost" class="hover_underline_animation items-end w-fit p-0 pb-1"
+              @click="onDelete(area.id)">
               <UIcon name="i-bytesize-trash" class="w-12 h-12 text-white" />
             </UButton>
           </UTooltip>
         </div>
       </UModal>
-      <UModal
-        v-model="confirmDeletionIsOpen[area.id]"
-        :ui="{
-          base: 'relative text-left rtl:text-right flex flex-col gap-10 p-10 border-custom_border_width',
-        }"
-        :style="{ borderColor: area.action.service.color }"
-      >
+      <UModal v-model="confirmDeletionIsOpen[area.id]" :ui="{
+        base: 'relative text-left rtl:text-right flex flex-col gap-10 p-10 border-custom_border_width',
+      }" :style="{ borderColor: area.action.service.color }">
         <h2 class="text-4xl font-semibold">
           Are you sure you want to delete this area?
         </h2>
         <p class="text-2xl">This action cannot be undone!</p>
         <div class="flex flex-row justify-end items-center gap-5 pt-5">
-          <UButton
-            class="bg-opacity-0 border-custom_border_width text-2xl font-semibold py-3 px-5"
-            :style="{
-              borderColor: area.action.service.color,
-              color: area.action.service.color,
-            }"
-            @click="cancelDeletion(area.id)"
-            >Cancel</UButton
-          >
-          <UButton
-            class="text-white text-2xl font-semibold py-3 px-5"
-            :style="{ backgroundColor: area.action.service.color }"
-            @click="onDelete(areaId)"
-            >Delete</UButton
-          >
+          <UButton class="bg-opacity-0 border-custom_border_width text-2xl font-semibold py-3 px-5" :style="{
+            borderColor: area.action.service.color,
+            color: area.action.service.color,
+          }" @click="cancelDeletion(area.id)">Cancel</UButton>
+          <UButton class="text-white text-2xl font-semibold py-3 px-5"
+            :style="{ backgroundColor: area.action.service.color }" @click="onDelete(area.id)">Delete</UButton>
         </div>
       </UModal>
     </div>
