51ef1e6903dbbb1b3c2aacefa8a2840a121b90e7
arthurgauffre <arth.gauffre@gmail.com>
2025-01-14 14:45:54 +0100

    [UPD] Refactor AreaDetails screen: remove unused PencilCircleOutline component, add area modification functionality with modal

--- mobile/src/screens/AreaDetails.tsx
+++ mobile/src/screens/AreaDetails.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import {
   View,
   Text,
@@ -8,21 +8,30 @@ import {
   TextInput,
 } from 'react-native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../Navigation/navigate';
-import MdiPencilCircleOutline from '../components/icons/PencilCircleOutline';
+import  { RootStackParamList } from '../Navigation/navigate';
+import { AppContext } from '../context/AppContext';
+import { SvgFromUri } from 'react-native-svg';
+import BottomNavBar from './NavBar';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'AreaDetails'>;
 
-const AreaDetailsScreen = ({ route }: Props) => {
+const AreaDetailsScreen = ({ navigation, route }: Props) => {
   const { area } = route.params;
+  const { ipAddress, token } = useContext(AppContext);
   const [isActionModalVisible, setIsActionModalVisible] = useState(false);
   const [isReactionModalVisible, setIsReactionModalVisible] = useState(false);
+  const [isAreaModalVisible, setIsAreaModalVisible] = useState(false);
   const [selectedActionOptions, setSelectedActionOptions] = useState<{
     [key: string]: any;
   }>({});
   const [selectedReactionOptions, setSelectedReactionOptions] = useState<{
     [key: string]: any;
   }>({});
+
+  const [title, setTitle] = useState<string>('');
+  const [description, setDescription] = useState<string>('');
+  const [refreshRate, setRefreshRate] = useState<number>();
+
 
   const handleActionOptionChange = (key: string, value: any, type: any) => {
     setSelectedActionOptions(prev => ({
@@ -38,13 +47,90 @@ const AreaDetailsScreen = ({ route }: Props) => {
     }));
   };
 
-  const handleSaveAction = () => {
-    console.log(selectedActionOptions);
+  const handleSaveArea = async () => {
+    console.log(title, description, refreshRate);
+    const newArea = {
+      ...area,
+      title: title,
+      description: description,
+      refresh_rate: refreshRate,
+    };
+    console.log(newArea);
+    try {
+      const response = await fetch(
+        `http://${ipAddress}:8080/api/v1/area`,
+        {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(newArea),
+        },
+      );
+      console.log('body:', JSON.stringify(newArea));
+      console.log(response);
+      if (response.ok) {
+        console.log('Area updated successfully');
+      }
+    } catch (error) {
+      console.error('Error update area:', error);
+    }
+    setIsAreaModalVisible(false);
+  }
+
+  const handleSaveAction = async () => {
+    const newArea = {
+      ...area,
+      action_option: selectedActionOptions,
+    };
+    try {
+      const response = await fetch(
+        `http://${ipAddress}:8080/api/v1/area`,
+        {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(newArea),
+        },
+      );
+      console.log(response);
+      if (response.ok) {
+        const body = await response.json();
+        setDescription(body.description);
+        setTitle(body.title);
+        setRefreshRate(body.refresh_rate);
+        console.log('Area updated successfully');
+      }
+    } catch (error) {
+      console.error('Error update area:', error);
+    }
     setIsActionModalVisible(false);
   };
 
-  const handleSaveReaction = () => {
-    console.log(selectedReactionOptions);
+  const handleSaveReaction = async () => {
+    const newArea = {
+      ...area,
+      reaction_option: selectedReactionOptions,
+    };
+    try {
+      const response = await fetch(
+        `http://${ipAddress}:8080/api/v1/area`,
+        {
+          method: 'PUT',
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(newArea),
+        },
+      );
+      console.log(response);
+      if (response.ok) {
+        console.log('Area updated successfully');
+      }
+    } catch (error) {
+      console.error('Error update area:', error);
+    }
     setIsReactionModalVisible(false);
   };
 
@@ -60,12 +146,41 @@ const AreaDetailsScreen = ({ route }: Props) => {
     selectedReactionOptions[option.name] = option.value;
   }
 
-  console.log(selectedActionOptions);
-
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Area Details</Text>
 
+      {/* Area Section */}
+      <View style={[styles.subContainer, { borderColor: 'black', borderWidth: 1 , borderRadius: 10}]}>
+        <View style={[{flexDirection: 'row', justifyContent: 'space-between'}]}>
+            <View style={[{flexDirection: 'column'}]}>
+            <View style={[styles.detailContainer, {flexDirection: 'column'}]}>
+              <Text style={[styles.label, {color: "black"}]}>title:</Text>
+              <Text style={[styles.value, {color: "black"}]}>{title == '' ? area.title : title}</Text>
+            </View>
+            <View style={[styles.detailContainer, {flexDirection: 'column'}]}>
+              <Text style={[styles.label, {color: "black"}]}>Description:</Text>
+              <Text style={[styles.value, {color: "black"}]}>{description == '' ? area.description : description}</Text>
+            </View>
+            <View style={styles.detailContainer}>
+              <Text style={[styles.label, {color: "black"}]}>Refresh rate:</Text>
+              <Text style={[styles.value, {color: "black"}]}>{refreshRate == undefined ? area.refresh_rate : refreshRate}</Text>
+            </View>
+          </View>
+          <View style={({alignContent: 'center'})}>
+            <TouchableOpacity onPress={() => setIsAreaModalVisible(true)}>
+              <SvgFromUri
+                uri={"https://api.iconify.design/mdi:pencil-circle-outline.svg"}
+                width={50}
+                height={50}
+                color={'black'}
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+      </View>
+      
       {/* Action Section */}
       <View
         style={[
@@ -78,15 +193,25 @@ const AreaDetailsScreen = ({ route }: Props) => {
         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
           <View>
             <View style={styles.detailContainer}>
-              <Text style={styles.label}>Action Service:</Text>
+              <Text style={styles.label}>Service:</Text>
               <Text style={styles.value}>{area.action.service.name}</Text>
             </View>
             <View style={styles.detailContainer}>
-              <Text style={styles.label}>Action:</Text>
+              <Text style={styles.label}>Options:</Text>
+              <Text style={styles.value}>
+                {Object.entries(selectedActionOptions).map(
+                  ([key, value]) => `${key}: ${value} `,
+                )}
+              </Text>
             </View>
           </View>
           <TouchableOpacity onPress={() => setIsActionModalVisible(true)}>
-            <MdiPencilCircleOutline size={50} color="white" />
+            <SvgFromUri
+              uri={"https://api.iconify.design/mdi:pencil-circle-outline.svg"}
+              width={50}
+              height={50}
+              color={'white'}
+            />
           </TouchableOpacity>
         </View>
       </View>
@@ -103,18 +228,95 @@ const AreaDetailsScreen = ({ route }: Props) => {
         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
           <View>
             <View style={styles.detailContainer}>
-              <Text style={styles.label}>Reaction Service:</Text>
+              <Text style={styles.label}>Service:</Text>
               <Text style={styles.value}>{area.reaction.service.name}</Text>
             </View>
             <View style={styles.detailContainer}>
-              <Text style={styles.label}>Reaction:</Text>
+              <Text style={styles.label}>Options:</Text>
+              <Text style={styles.value}>
+                {Object.entries(selectedReactionOptions).map(
+                  ([key, value]) => `${key}: ${value} `,
+                )}
+              </Text>
             </View>
           </View>
           <TouchableOpacity onPress={() => setIsReactionModalVisible(true)}>
-            <MdiPencilCircleOutline size={50} color="white" />
+            <SvgFromUri
+              uri={"https://api.iconify.design/mdi:pencil-circle-outline.svg"}
+              width={50}
+              height={50}
+              color={'white'}
+            />
           </TouchableOpacity>
         </View>
       </View>
+
+      {/* Area Modal */}
+      <Modal
+        visible={isAreaModalVisible}
+        transparent={true}
+        animationType="slide">
+        <View style={styles.modalContainer}>
+          <View
+            style={[
+              styles.modalContent,
+              { backgroundColor: 'white', borderColor: 'black', borderWidth: 1, borderRadius: 10 },
+            ]}>
+            <Text style={[styles.modalHeader, {color: 'black'}]}>Modify Area</Text>
+            <View style={[{ flexDirection: 'column' }]}>
+              <View style={[styles.optionRow]}>
+                <Text style={[styles.optionLabel, {color: 'black'}]}>Title</Text>
+                <TextInput
+                  style={styles.optionInput}
+                  value={title}
+                  onChangeText={text => setTitle(text)}
+                  keyboardType="default" // Adjust as needed
+                />
+              </View>
+              <View style={[styles.optionRow]}>
+                <Text style={[styles.optionLabel, {color: 'black'}]}>Description</Text>
+                <TextInput
+                  style={styles.optionInput}
+                  value={description}
+                  onChangeText={text => (setDescription(text))}
+                  keyboardType="default" // Adjust as needed
+                />
+              </View>
+              <View style={[styles.optionRow]}>
+                <Text style={[styles.optionLabel, {color: 'black'}]}>Refresh rate</Text>
+                <TextInput
+                  style={[styles.optionInput]}
+                  value={refreshRate ? String(refreshRate) : ""}
+                  onChangeText={text => (setRefreshRate(Number(text)))}
+                  keyboardType="numeric" // Adjust as needed
+                />
+              </View>
+            </View>
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <TouchableOpacity onPress={handleSaveArea}>
+                <View style={[styles.saveButton, { borderColor: 'black' }]}>
+                  <Text style={[{ color: 'black' }, { fontSize: 16 }]}>
+                    Save
+                  </Text>
+                </View>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {
+                setIsAreaModalVisible(false);
+                setDescription(area.description);
+                setTitle(area.title);
+                setRefreshRate(area.refresh_rate);
+              }}>
+                <View style={styles.cancelButton}>
+                  <Text style={[{ color: 'red' }, { fontSize: 16 }]}>
+                    Cancel
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
 
       {/* Action Modal */}
       <Modal
@@ -220,6 +422,7 @@ const AreaDetailsScreen = ({ route }: Props) => {
           </View>
         </View>
       </Modal>
+      <BottomNavBar navigation={navigation}/>
     </View>
   );
 };
