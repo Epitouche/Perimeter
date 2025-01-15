7d3f78cd13d21f30057c1419aab63dbe10d382a3
arthurgauffre <arth.gauffre@gmail.com>
2025-01-15 11:02:09 +0100

    [ADD] Refactor AreaDetails screen and extract ReactionsSections component

--- mobile/src/screens/AreaDetails/AreaDetails.tsx
+++ mobile/src/screens/AreaDetails/AreaDetails.tsx
@@ -15,57 +15,16 @@ import { SvgFromUri } from 'react-native-svg';
 import { styles } from './StylesAreaDetails';
 import AreaSections from './AreaSections';
 import ActionsSections from './ActionsSections';
+import ReactionsSections from './ReactionsSections';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'AreaDetails'>;
 
 const AreaDetailsScreen = ({ navigation, route }: Props) => {
   const { area } = route.params;
   const { ipAddress, token } = useContext(AppContext);
-  const [isReactionModalVisible, setIsReactionModalVisible] = useState(false);
-  const [selectedReactionOptions, setSelectedReactionOptions] = useState<{
-    [key: string]: any;
-  }>({});
-  console.log('Area refreshrate:', area.action_refresh_rate);
-
   const [areaResults, setAreaResults] = useState([
     { created_at: '', result: '' },
   ]);
-
-  const handleReactionOptionChange = (key: string, value: any, type: any) => {
-    setSelectedReactionOptions(prev => ({
-      ...prev,
-      [key]: type === 'number' ? Number(value) : value,
-    }));
-  };
-
-  const handleSaveReaction = async () => {
-    const newArea = {
-      ...area,
-      reaction_option: selectedReactionOptions,
-    };
-    try {
-      const response = await fetch(`http://${ipAddress}:8080/api/v1/area`, {
-        method: 'PUT',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(newArea),
-      });
-      console.log(response);
-      if (response.ok) {
-        console.log('Area updated successfully');
-      }
-    } catch (error) {
-      console.error('Error update area:', error);
-    }
-    setIsReactionModalVisible(false);
-  };
-
-  for (const option of Object.entries(area.reaction_option).map(
-    ([name, value]) => ({ name, value }),
-  )) {
-    selectedReactionOptions[option.name] = option.value;
-  }
 
   useEffect(() => {
     const fetchAreaResults = async () => {
@@ -115,40 +74,7 @@ const AreaDetailsScreen = ({ navigation, route }: Props) => {
         {ActionsSections({navigation, route})}
 
         {/* Reaction Section */}
-        <View
-          style={[
-            styles.subContainer,
-            { backgroundColor: area.reaction.service.color },
-          ]}>
-          <View style={styles.ActionReactionHeader}>
-            <Text style={styles.label}>Reaction</Text>
-          </View>
-          <View
-            style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-            <View>
-              <View style={styles.detailContainer}>
-                <Text style={styles.label}>Service:</Text>
-                <Text style={styles.value}>{area.reaction.service.name}</Text>
-              </View>
-              <View style={styles.detailContainer}>
-                <Text style={styles.label}>Options:</Text>
-                <Text style={styles.value}>
-                  {Object.entries(selectedReactionOptions).map(
-                    ([key, value]) => `${key}: ${value} `,
-                  )}
-                </Text>
-              </View>
-            </View>
-            <TouchableOpacity onPress={() => setIsReactionModalVisible(true)}>
-              <SvgFromUri
-                uri={'https://api.iconify.design/mdi:pencil-circle-outline.svg'}
-                width={50}
-                height={50}
-                color={'white'}
-              />
-            </TouchableOpacity>
-          </View>
-        </View>
+        {ReactionsSections({navigation, route})}
 
         {/* Area Results */}
         <Text style={[styles.header, { marginTop: 16 }]}>Area Results</Text>
@@ -168,61 +94,6 @@ const AreaDetailsScreen = ({ navigation, route }: Props) => {
           />
         </View>
 
-        {/* Reaction Modal */}
-        <Modal
-          visible={isReactionModalVisible}
-          transparent={true}
-          animationType="slide">
-          <View style={styles.modalContainer}>
-            <View
-              style={[
-                styles.modalContent,
-                { backgroundColor: area.reaction.service.color },
-              ]}>
-              <Text style={styles.modalHeader}>Modify Reaction</Text>
-              <View style={[{ flexDirection: 'column' }]}>
-                {Object.keys(selectedReactionOptions).map(key => (
-                  <View key={key} style={styles.optionRow}>
-                    <Text style={styles.optionLabel}>{key}</Text>
-                    <TextInput
-                      style={styles.optionInput}
-                      value={String(selectedReactionOptions[key])}
-                      onChangeText={text =>
-                        handleReactionOptionChange(
-                          key,
-                          text,
-                          typeof selectedReactionOptions[key],
-                        )
-                      }
-                      keyboardType="default" // Adjust as needed
-                    />
-                  </View>
-                ))}
-              </View>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  justifyContent: 'space-between',
-                }}>
-                <TouchableOpacity onPress={handleSaveReaction}>
-                  <View style={styles.saveButton}>
-                    <Text style={[{ color: 'white' }, { fontSize: 16 }]}>
-                      Save
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-                <TouchableOpacity
-                  onPress={() => setIsReactionModalVisible(false)}>
-                  <View style={styles.cancelButton}>
-                    <Text style={[{ color: '#E60000' }, { fontSize: 16 }]}>
-                      Cancel
-                    </Text>
-                  </View>
-                </TouchableOpacity>
-              </View>
-            </View>
-          </View>
-        </Modal>
         <TouchableOpacity onPress={() => navigation.navigate('AreaView')}>
           <View style={{ alignItems: 'flex-end', justifyContent: 'flex-end' }}>
             <Text
