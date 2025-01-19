6ede37a33bb269c1fe08a4c2e6b1d6ad8733245d
Tom Mendy <tom.mendy@epitech.eu>
2025-01-19 21:11:57 +0100

    [FIX] Update email validation regex to allow uppercase letters

--- backend/service/user-service.go
+++ backend/service/user-service.go
@@ -71,7 +71,7 @@ func NewUserService(userRepository repository.UserRepository, serviceJWT JWTServ
 // Returns:
 //   - bool: true if the email is valid, false otherwise.
 func isValidEmail(email string) bool {
-	re := regexp.MustCompile(`^[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$`)
+	re := regexp.MustCompile(`^[a-zA-Z0-9._%+\-]+@[a-zA-Z0-9.\-]+\.[a-z]{2,}$`)
 	return re.MatchString(email)
 }
 
